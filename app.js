4e0d9b82a9ec777a0a4cbf1d5def49827ed02108
root <root@localhost.localdomain>
2015-05-14 13:44:06 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<title>프로젝트</title>
+</head>
+<body>
+<p> 태그로 작성된 문단 </p>
+
+<script>
+document.write("안녕하세요! 자바스크립트");
+<script>
+
+</body>
+</html>
+
